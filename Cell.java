9d667ace38c890e538be005ad74f2b037482708b
mtsir1 <36860233+mtsir1@users.noreply.github.com>
2018-03-01 15:20:24 -0500

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -6,8 +6,11 @@ public class Cell extends JLabel {
 	//Cell class for each individual cell that is a part of LabelArray
 	private static final long serialVersionUID = -3776485612595454858L;
 	//no clue what the serialVersionUID is, Java gave me a warning to put it there.
-	/*static Color[] colorArray = {new Color(145, 249, 97), new Color(6, 173, 134), 
-			new Color(106, 69, 109), new Color(237, 0, 71), new Color(229, 128, 84), new Color(247, 233, 76)};*/
+	static Color[] hardColorArray = {new Color(145, 249, 97), new Color(6, 173, 134), 
+			new Color(106, 69, 109), new Color(237, 0, 71), new Color(229, 128, 84), new Color(247, 233, 76)};
+	
+	static Color[] medColorArray = {new Color(145, 249, 97), new Color(6, 173, 134), 
+	new Color(106, 69, 109), new Color(237, 0, 71)};
 	
 	static Color[] colorArray = {new Color(145, 249, 97), new Color(6, 173, 134), 
 			new Color(106, 69, 109)};
@@ -21,11 +24,12 @@ public class Cell extends JLabel {
 	final Dimension dimSize = new Dimension(20, 20);
 	//size of cell
 	public boolean isActive;
-	//whether or not the cell si active
+	//whether or not the cell is active
 	public Cell() {
 		rnd = new Random();
-		rndIndex = rnd.nextInt(colorArray.length);
+		
 		c2 = colorArray[rndIndex];
+		
 		this.setBackground(c2);
 		//sets the background of a cell to one of the colors in colorArray
 		this.setSize(dimSize);
@@ -36,13 +40,28 @@ public class Cell extends JLabel {
 		isActive = false;
 		//initially all the cells are inactive
 	}
-	public Cell[][] toLabelArray(int dim1Length, int dim2Length) {
+	public static Cell[][] toLabelArray(Cell [][] c, int dim1Length, int dim2Length, String dif) {
 		//takes a cell and makes a whole array of them
-		Cell[][] c = new Cell [dim1Length][dim2Length];
+		//Cell[][] c = new Cell [dim1Length][dim2Length];
+		int rndIndex;
+		Random rnd;
+		Color c2;
 		
+		if (dif.equalsIgnoreCase("Easy")) {
+			rndIndex = rnd.nextInt(colorArray.length);
+			c2 = colorArray[rndIndex];
+		} else if (dif.equalsIgnoreCase("Medium")) {
+			rndIndex = rnd.nextInt(medColorArray.length);
+			c2 = medColorArray[rndIndex];
+		} else if (dif.equalsIgnoreCase("Hard")) {
+			rndIndex = rnd.nextInt(hardColorArray.length);
+			c2 = hardColorArray[rndIndex];
+		}
+		
+		this.setBackground(c2);
 		for (int i = 0; i < c[0].length; i++) {
 			for (int j = 0; j < c[1].length; j++) {
-				c[i][j] = new Cell();
+				c[i][j] = new Cell(dif);
 			}
 		}
 		return c;
