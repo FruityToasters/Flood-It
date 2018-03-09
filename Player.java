236e5ec8db89995696882d2358b895496e5b1784
mtsir1 <36860233+mtsir1@users.noreply.github.com>
2018-03-08 22:55:12 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -11,15 +11,29 @@ public class Player {
 	
 	public void getStars() {
 		if (this.difficulty.equalsIgnoreCase("easy")) {
-			if (this.score <= 6) {
+			if (this.score <= 17) {
 				this.stars = "Three stars! ***";
-			} else if (this.score > 6 && this.score <= 8) {
+			} else if (this.score > 17 && this.score <= 20) {
 				this.stars = "Two stars! **";
 			} else {
 				this.stars = "Only one star *";
 			}
 		} else if (this.difficulty.equalsIgnoreCase("medium")) {
-			
+			if (this.score <= 34) {
+				this.stars = "Three stars! ***";
+			} else if (this.score > 34 && this.score <= 38) {
+				this.stars = "Two stars! **";
+			} else {
+				this.stars = "Only one star *";
+			}
+		} else {
+			if (this.score <= 49) {
+				this.stars = "Three stars! ***";
+			} else if (this.score > 49 && this.score <= 53) {
+				this.stars = "Two stars! **";
+			} else {
+				this.stars = "Only one star *";
+			}
 		}
 	}
 	
